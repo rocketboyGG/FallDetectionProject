9b3b2d0a611cf32882e592b9ee33df20ba5b5d5f
Theodor <thgr0003@stud.kea.dk>
2025-12-11 09:52:56 +0100

    Gjort det muligt at læse data fra databasen og vise det på hjemmeside

--- Webpage/app.py
+++ Webpage/app.py
@@ -1,21 +1,68 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from flask_login import LoginManager, login_user, login_required, current_user, logout_user, UserMixin
 import paho.mqtt.client as mqtt
 import psycopg2
+import json
 
 conn = psycopg2.connect(
     host="localhost",
     database="falldetectDatabase",
     user="postgres",
-    password="123qwe"
+    password="GGcakerocket87GG"
 )
 cursor = conn.cursor()
+
+def create_test_database(cursor, conn):
+    sql = """ 
+        CREATE TABLE sensor_data (
+            id SERIAL PRIMARY KEY,
+            topic TEXT NOT NULL,
+            payload TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT NOW()
+        );
+        """
+    cursor.execute(sql)
+    conn.commit()
+
+def create_test_data(cursor, conn):
+    cursor.execute(
+        "INSERT INTO sensor_data (topic, payload) VALUES (%s, %s)",
+        ("fallband/fall", "FALL")
+    )
+    cursor.execute(
+        "INSERT INTO sensor_data (topic, payload) VALUES (%s, %s)",
+        ("fallband/pulse", "23")
+    )
+    cursor.execute(
+        "INSERT INTO sensor_data (topic, payload) VALUES (%s, %s)",
+        ("fallband/battery", "46")
+    )
+    conn.commit()
+
+create_test_data(cursor, conn)
+
+def print_table(cursor, conn):
+    sql1 = """
+        SELECT payload
+        FROM sensor_data
+        WHERE topic = 'fallband/pulse'
+        ORDER BY created_at DESC
+        LIMIT 1;
+    """
+    cursor.execute(sql1)
+    pulse_data = cursor.fetchone()
+    print(pulse_data)
+
+print_table(cursor, conn)
+
 
 def on_message(client, userdata, msg):
     cursor = userdata["cursor"]
     conn = userdata["conn"]
     topic = msg.topic
     payload = msg.payload.decode()
+    if "fallband/fall" in topic:
+        client.publish(b"fallband/vibrator", "FALL")
     cursor.execute(
         "INSERT INTO sensor_data (topic, payload) VALUES (%s, %s)",
         (topic, payload)
@@ -24,15 +71,14 @@ def on_message(client, userdata, msg):
 
 def mqtt_runner(db_cursor, db_conn):
     client = mqtt.Client()
-    client.on_message() = on_message
+    client.on_message = on_message
     client.connect("192.168.68.64")
     client.user_data_set({"cursor": db_cursor, "conn": db_conn})
     client.subscribe("fallband/battery")
     client.subscribe("fallband/pulse")
-    client.subscribe("fallband/temp")
     client.subscribe("fallband/fall")
     client.loop_start()
-mqtt_runner(cursor, conn)
+#mqtt_runner(cursor, conn)
 
 app = Flask(__name__)
 app.secret_key = "secret" #Skal ændres senere, just for developent/debugging
@@ -40,6 +86,8 @@ app.secret_key = "secret" #Skal ændres senere, just for developent/debugging
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
+
+last_fall = None
 
 class User(UserMixin):
     def __init__(self, id):
@@ -52,15 +100,57 @@ TEMP_PASSWORD = "123"
 def load_user(user_id):
     return User(user_id)
 
+def fetch_data_from_db():
+    sql1 = """
+        SELECT payload
+        FROM sensor_data
+        WHERE topic = 'fallband/pulse'
+        ORDER BY created_at DESC
+        LIMIT 1;
+    """
+    cursor.execute(sql1)
+    pulse_data = cursor.fetchone()
+
+    sql2 = """
+        SELECT payload
+        FROM sensor_data
+        WHERE topic = 'fallband/battery'
+        ORDER BY created_at DESC
+        LIMIT 1;
+    """
+    cursor.execute(sql2)
+    battery_data = cursor.fetchone()
+
+    global last_fall
+    sql3 = """
+        SELECT created_at
+        FROM sensor_data
+        WHERE topic = 'fallband/fall'
+        ORDER BY created_at DESC
+        LIMIT 1;
+    """
+    cursor.execute(sql3)
+    fall_registered = cursor.fetchone()
+
+    if fall_registered != last_fall:
+        print("!!!!!FALLLLLLL!!!!!!!")
+        last_fall = fall_registered
+        return (pulse_data[0], battery_data[0], fall_registered[0])
+
+    return (pulse_data[0], battery_data[0], "no")
+
+
 @app.route("/home")
 @login_required
 def home():
-    temp_list = [
-        {"navn": "Navn 1", "t": "31", "p": "1", "status": "Good"},
-        {"navn": "Rigtig langt navn !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", "t": "32", "p": "2", "status": "uhh"},
-        {"navn": "Navn 3", "t": "33", "p": "3", "status": "Bad"},
-    ]
-    return render_template("home.html", temp_list=temp_list)
+    return render_template("home.html")
+
+@app.route('/data')
+def api_data():
+    """API endpoint to return the visualization data as JSON (for JS updates)."""
+    data = fetch_data_from_db()
+    print(data)
+    return jsonify(data)
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -82,7 +172,6 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for("login"))
-
 
 if __name__ == ('__main__'):
     app.run(host="0.0.0.0", debug=True)
